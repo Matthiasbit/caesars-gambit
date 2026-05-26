121da47164867ca1e0444169b1d07a5b98e235b9
knoepsim <simon.info.jc@gmail.com>
2026-05-26 14:06:24 +0200

    settings page

---
+++ Backend/src/main/java/com/risiko/model/dto/UpdateUsernameRequest.java
@@ -0,0 +1,20 @@
+package com.risiko.model.dto;
+
+public class UpdateUsernameRequest {
+    private String username;
+
+    public UpdateUsernameRequest() {
+    }
+
+    public UpdateUsernameRequest(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
